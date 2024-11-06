124d11c03cbdc3547a1768031d325824dbfab6b4
kairosir <44501534+kairosir@users.noreply.github.com>
2024-11-07 00:15:40 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lampy-creative-space-32/',
+  base: '/lampahub/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
