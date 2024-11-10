24d0735dcbda2869d216865a6265cc6f5aec0429
kairosir <44501534+kairosir@users.noreply.github.com>
2024-11-10 15:54:05 +0500

    Update Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,7 +10,7 @@ const Map = () => {
         
         <div className="rounded-2xl overflow-hidden shadow-lg">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2710.5677389757544!2d39.7503!3d47.1384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3b9b31c471913%3A0x4d5f7c5c6c731f68!2z0YPQuy4g0JvQtdC90LjQvdCwLCAzLCDQkdCw0YLQsNC50YHQuiwg0KDQvtGB0YLQvtCy0YHQutCw0Y8g0L7QsdC7Liwg0KDQvtGB0YHQuNGPLCAzNDY4ODA!5e0!3m2!1sru!2s!4v1647887291254!5m2!1sru!2s"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d678.5225266252721!2d39.74471906966492!3d47.13650071048894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDfCsDA4JzExLjQiTiAzOcKwNDQnNDMuMyJF!5e0!3m2!1sru!2skz!4v1731235981294!5m2!1sru!2skz"
             width="100%"
             height="450"
             style={{ border: 0 }}
